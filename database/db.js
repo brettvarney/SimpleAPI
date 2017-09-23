4ecd3549520e6ea612065f7a1e597b2931df890b
brettvarney <bvarney@xceligent.com>
2017-09-22 19:14:35 -0500

    read/parse DB file into in-memory "table array

--- database/db.js
+++ database/db.js
@@ -8,11 +8,28 @@ const fs = require('fs');
 const databaseFile = 'database/pharmacies.csv';
 const encoding = 'utf8';
 
+// read CSV file; parse into array "table" and column names
+let parseDb = function() {
+  let data = fs.readFileSync(databaseFile, encoding, (err, contents) => {
+    if (err) throw err;
+    return contents;
+  });
+
+  if (!data) {
+    throw 'No data in database.';
+  }
+  data = data.replace(/[\\"]/g, '').split('\n');  // make into "table" array, strip backslash-quote combos
+  let cols = data[0].split('\n')[0].split(',');
+  return JSON.stringify(cols[0]);
+}
+
+// verify existence of critical column names for search/results
+let verifyData = function(){
+  
+}
+
 module.exports = {
   getList: function() {
-    return fs.readFileSync(databaseFile, encoding, (err, contents) => {
-      if (err) throw err;
-      return contents;
-    })
+    return parseDb();
   }
 }
--- server.js
+++ server.js
@@ -16,10 +16,11 @@ app.get('/', function(request, response) {
     response.send(database.getList());
   } catch (err) {
     console.error(err);
+    response.status(500).send("I'm afraid I can't do that. It's not you, it's me.\n\n")
   }
 });
 
 /* server */
 const server = app.listen(serverPort, () =>
-  console.log(`Server started, listening on port ${server.address().port}...`) // to verify port is correct
+  console.log(`Server started, listening on port ${server.address().port}...\n\n`) // to verify port is correct
 );
