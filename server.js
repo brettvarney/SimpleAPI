a9ebbc50740c58acc6730ae6b30577071af731be
brettvarney <bvarney@xceligent.com>
2017-09-23 15:59:48 -0500

    only return needed values

--- server.js
+++ server.js
@@ -14,21 +14,28 @@ var database = new Database; // initialize database
 
 /* routing */
 app.get('/', function(request, response) {
-  let lat = request.query.lat || request.query.latitude;
-  if (lat) {
-    let long = request.query.long || request.query.longitude;
-    if (long) {
-      try {
-        response.send(JSON.stringify(database.findNearest(lat, long)));
-      } catch (err) {
-        console.error(err);
-        response.status(500).send("I'm afraid I can't do that. It's not you, it's me.\n\n")
+  try {
+    let lat = request.query.lat || request.query.latitude;
+    if (lat) {
+      let long = request.query.long || request.query.longitude;
+      if (long) {
+
+        // get only specific properties to return to requester
+        let {name, address, distance} = database.findNearest(lat, long);
+        response.json({
+          "name": name,
+          "address": address,
+          "distance": distance
+        });
+      } else {
+        response.status(422).end('Required parameter missing: "long" (or "longitude")\n\n');
       }
     } else {
-      response.status(422).send('Required parameter missing: "long" (or "longitude")');
+      response.status(422).end('Required parameter missing: "lat" (or "latitude")\n\n');
     }
-  } else {
-    response.status(422).send('Required parameter missing: "lat" (or "latitude")');
+  } catch (err) {
+    console.error(err);
+    response.status(500).end("I'm afraid I can't do that. It's not you, it's me.\n\n")
   }
 });
 
